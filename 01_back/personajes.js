37891ab1a2437b4006b1426d68945b8c3aff9d65
MarianelaAltamirano <127756206+MarianelaAltamirano@users.noreply.github.com>
2024-12-27 14:41:14 -0300

    variables

---
+++ 01_back/personajes.js
@@ -0,0 +1,32 @@
+//EJERCITACIÓN: ESCRIBIRI DOS PERSONAJES DE FICCION, CON SUS DATOS Y LUEGO A LA EDAD SUMARLE 5
+
+
+const nombre1 = "Peter Parker"
+const ciudad1 = "New York"
+const habilidades1 = ["super fuerza", "agilidad", "reflejos"]
+//const edad1 = 18 
+//modifico el const por let
+let edad1 = 18
+console.log(nombre1);
+console.log(ciudad1);
+console.log(habilidades1);
+//edad1 = edad1+5
+edad1 +=5 //otra forma de sumar 5
+console.log(edad1)
+
+const nombre2 = "Natasha Romanova"
+const ciudad2 = "New York"
+const habilidades2 = ["maestra de armas", "tirador escondido", "espía experta"]
+const edad2 = 30
+console.log(nombre2);
+console.log(ciudad2);
+console.log(habilidades2);
+//const edad2 = 18 
+//modifico el const por let
+//edad2 = edad2+5 
+edad2 +=5  
+//otra forma de sumar 5
+console.log(edad2)
+
+
+
